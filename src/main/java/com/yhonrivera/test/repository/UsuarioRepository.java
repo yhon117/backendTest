2d2d06910f1cbf61d8137f45c5b898af27510702
yhon 117 <jhonbaironrivera2004@gmail.com>
2024-07-14 03:54:55 -0500

    se alladio el metodo update y delete

--- src/main/java/com/yhonrivera/test/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yhonrivera.test.repository;
-
-import com.yhonrivera.test.model.Usuario;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
-}
